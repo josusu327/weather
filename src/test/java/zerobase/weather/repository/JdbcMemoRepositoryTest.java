a3518f5b5e44caf9049825497a6911caaacc3bd4
조수빈 <whtnqls03@gmail.com>
2025-02-04 16:53:29 +0900

    first commit

---
+++ src/test/java/zerobase/weather/repository/JdbcMemoRepositoryTest.java
@@ -0,0 +1,47 @@
+package zerobase.weather.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.domain.Memo;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@SpringBootTest
+@Transactional
+class JdbcMemoRepositoryTest {
+
+    @Autowired
+    JdbcMemoRepository jdbcMemoRepository;
+
+    @Test
+    void insertMemoTest() {
+        // given
+        Memo memo = new Memo(2, "insert Memo Test");
+
+        // when
+        jdbcMemoRepository.save(memo);
+
+        // then
+        Optional<Memo> result = jdbcMemoRepository.findById(2);
+        assertEquals(result.get().getText(), "insert Memo Test");
+    }
+
+    @Test
+    void findAllMemoTest() {
+        // given
+        List<Memo> memoList = jdbcMemoRepository.findAll();
+        // when
+        System.out.println(memoList);
+        // then
+        assertNotNull(memoList);
+    }
+
+
+}
